9dda3a29ce8b7db4a7672f8891220628748d2a82
Игорь Честа <igorche2760@gmail.com>
2024-08-30 01:06:07 +0300

    pytest acomplished

---
+++ blogicum/blogicum/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+    path('posts/', include('blog.urls')),
+    path('category/', include('blog.urls')),
+    path('pages/', include('pages.urls')),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
